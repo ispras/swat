ffc419c3c8457e487e0ec1c14a05d7cf50bb0008
Pavel Dovgalyuk <pavel.dovgaluk@gmail.com>
2019-02-27 10:52:34 +0300

    Added build script

---
+++ rebuild.sh
@@ -0,0 +1,14 @@
+dir=$(pwd)/release
+mkdir build
+mkdir -p release/bin
+cd build
+mkdir qemu-gui
+cd qemu-gui
+cmake ../../qemu-gui
+make
+cp qemu-gui $dir/bin
+cd ..
+mkdir qemu
+cd qemu
+../../qemu/configure --target-list="i386-softmmu,x86_64-softmmu,arm-softmmu,aarch64-softmmu,mips-softmmu,mips64-softmmu" --enable-sdl --disable-kvm --prefix=$dir
+make install
