ce54a9c93471e57d3af112cb27ef5cd81219cb27
Pavel Dovgalyuk <Pavel.Dovgaluk@ispras.ru>
2019-02-28 14:08:22 +0300

    Build script update

--- rebuild.sh
+++ rebuild.sh
@@ -1,14 +1,16 @@
 dir=$(pwd)/release
-mkdir build
-mkdir -p release/bin
+mkdir -p build
 cd build
-mkdir qemu-gui
-cd qemu-gui
-cmake ../../qemu-gui
-make
-cp qemu-gui $dir/bin
-cd ..
-mkdir qemu
+
+# build qemu
+
+mkdir -p qemu
 cd qemu
 ../../qemu/configure --target-list="i386-softmmu,x86_64-softmmu,arm-softmmu,aarch64-softmmu,mips-softmmu,mips64-softmmu" --enable-sdl --disable-kvm --enable-plugins --prefix=$dir
+make install
+cd ..
+
+# build everything else
+
+cmake -DCMAKE_INSTALL_PREFIX=../release ..
 make install
