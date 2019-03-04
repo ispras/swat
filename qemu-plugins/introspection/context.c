a7f26c4bfe7e26f3dd4c77188afc4a3d037500f8
Pavel Dovgalyuk <Pavel.Dovgaluk@ispras.ru>
2019-03-04 11:45:56 +0300

    Added stack pointer introspection function

--- qemu-plugins/introspection/context.c
+++ qemu-plugins/introspection/context.c
@@ -8,3 +8,10 @@ context_t vmi_get_context(cpu_t cpu)
     qemulib_read_register(cpu, (uint8_t*)&reg, I386_CR3_REGNUM);
     return reg;
 }
+
+uint64_t vmi_get_stack_pointer(cpu_t cpu)
+{
+    uint32_t reg;
+    qemulib_read_register(cpu, (uint8_t*)&reg, I386_ESP_REGNUM);
+    return reg;
+}
--- qemu-plugins/introspection/introspection.h
+++ qemu-plugins/introspection/introspection.h
@@ -8,5 +8,6 @@ typedef void * cpu_t;
 /* Execution context */
 typedef uint64_t context_t;
 context_t vmi_get_context(cpu_t cpu);
+uint64_t vmi_get_stack_pointer(cpu_t cpu);
 
 #endif // INTROSPECTION_H
